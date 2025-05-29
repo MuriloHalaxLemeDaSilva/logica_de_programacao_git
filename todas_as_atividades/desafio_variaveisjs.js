7242bd5109dd4eedbffd41bd8da9fcf27fb1de6d
murilo <murilohalax262@gmail.com>
2025-05-29 11:06:35 -0300

    desafio variaveis

---
+++ todas_as_atividades/desafio_variaveisjs.js
@@ -0,0 +1,2 @@
+https://drive.google.com/file/d/1rEoJcVzp4thzHW_VmsS732AviM4rmuPS/view?usp=classroom_web&authuser=0
+https://drive.google.com/file/d/1pLKbTyZ8qbzKE2cgexhkxGVJhmDQyiVe/view?usp=classroom_web&authuser=0
