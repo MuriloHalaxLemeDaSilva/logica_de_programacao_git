d52343d9de35683cad05dd71a5183540784c05ec
murilo <murilohalax262@gmail.com>
2025-05-29 11:02:09 -0300

    atividade1

---
+++ todas_as_atividades/atividade1_variaveis.js
@@ -0,0 +1,34 @@
+let nome = "Murilo"
+let idade = 16
+console.log(idade)
+
+let aprendendojavascript = "true"
+console.log(aprendendojavascript)
+
+let sobrenome = " Halax"
+let nomecompleto = (nome + sobrenome)
+console.log(nomecompleto)
+
+let numero1 = 1229
+let numero2 = 34
+let soma = numero1 + numero2
+console.log(soma)
+
+let multiplicação = numero1 * numero2
+console.log(multiplicação)
+
+let cidade = "ferraz de v.= "
+let habitantes = 179.198
+console.log(cidade + habitantes)
+
+var preco = 1234
+console.log(preco)
+var preco = 4321
+console.log(preco)
+
+var meuvalor
+console.log(meuvalor)
+
+var meuNulo = null
+console.log(meuNulo)
+ECHO est  ativado.
